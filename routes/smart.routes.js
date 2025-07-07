ba4559ce1e8fcbd36bede1adcdcefcef1a4fa3e8
RaviGupta965 <clicktoravi1234@gmail.com>
2025-07-08 00:37:24 +0530

    second-commit

--- routes/smart.routes.js
+++ routes/smart.routes.js
@@ -1,3 +1,9 @@
+import express from "express";
 import { smartAssign } from "../controllers/Assign.controller.js";
+import { authMiddleware } from "../middleware/auth.js";
+
+const router = express.Router();
 
 router.post("/:taskId/smart-assign", authMiddleware, smartAssign);
+
+export default router;
