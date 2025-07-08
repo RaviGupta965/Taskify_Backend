6110b0209041c0105271656ed1f81395e26cfe38
RaviGupta965 <clicktoravi1234@gmail.com>
2025-07-09 01:36:05 +0530

    origin-updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // your frontend origin
+    origin: "https://taskify-frontend-sooty.vercel.app/", 
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
